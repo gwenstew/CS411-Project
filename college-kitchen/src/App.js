2c43141ea445eebba9206d68b773a7853ead2d82
diyagandhi15 <123668392+diyagandhi15@users.noreply.github.com>
2024-03-03 20:06:09 -0500

    login page

--- college-kitchen/src/App.js
+++ college-kitchen/src/App.js
@@ -1,8 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './home'
+import Login from './login'
+import './App.css'
+import { useEffect, useState } from 'react'
+
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [email, setEmail] = useState('')
   return (
     <div className="App">
-    <h1> Hello </h1>
-      
+      <h1> Hello World </h1>
+      <h2> Static Web Page </h2>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home email={email} loggedIn={loggedIn} setLoggedIn={setLoggedIn} />} />
+          <Route path="/login" element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
