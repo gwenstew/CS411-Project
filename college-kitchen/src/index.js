79c730a7ca573f06c08a97f959ab78a109087d8d
diyagandhi15 <123668392+diyagandhi15@users.noreply.github.com>
2024-03-04 15:40:00 -0500

    added database coniguration

--- college-kitchen/src/index.js
+++ college-kitchen/src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from "firebase/analytics";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,16 +11,3 @@ root.render(
   </React.StrictMode>
 );
 
-const firebaseConfig = {
-  apiKey: "AIzaSyArNaWNiI7dlCQZi1b9lIEQa4pDsroW6KE",
-  authDomain: "college-kitchen-bu.firebaseapp.com",
-  databaseURL: "https://college-kitchen-bu-default-rtdb.firebaseio.com",
-  projectId: "college-kitchen-bu",
-  storageBucket: "college-kitchen-bu.appspot.com",
-  messagingSenderId: "646410487427",
-  appId: "1:646410487427:web:9381fd24abc60bc7893db6",
-  measurementId: "G-KRB5DSBN00"
-};
-
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
