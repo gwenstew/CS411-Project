a58d5aa4ea48296063596d6b7112be8924c3ad75
diyagandhi15 <123668392+diyagandhi15@users.noreply.github.com>
2024-03-03 19:51:57 -0500

    static web page

--- college-kitchen/src/App.js
+++ college-kitchen/src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      {/*API CALL*/}
-      /* fetch("spoonacular.com/api&key="process.env.SPOONACULAR") */
+    <h1> Hello </h1>
+      
     </div>
   );
 }
