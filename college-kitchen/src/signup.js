da299cebffd00ece53dc53562206b2ca7f527537
diyagandhi15 <123668392+diyagandhi15@users.noreply.github.com>
2024-03-05 09:48:26 -0500

    working on firebase user authentication

---
+++ college-kitchen/src/signup.js
@@ -0,0 +1,14 @@
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const auth = getAuth();
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed up 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
