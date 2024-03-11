362c803e4b50edb8727aeb49b4b0ba557c0ca927
diyagandhi15 <123668392+diyagandhi15@users.noreply.github.com>
2024-03-11 18:56:26 -0400

    removed line that was causing app to not launch

--- college-kitchen/src/login.js
+++ college-kitchen/src/login.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { formatResponseError } from '@ionic/cli/lib/http'
 
 const Login = (props) => {
   const [email, setEmail] = useState('')
