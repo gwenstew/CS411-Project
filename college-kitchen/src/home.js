120e15c268f2f49ff3757a13a88e727eaf948387
diyagandhi15 <123668392+diyagandhi15@users.noreply.github.com>
2024-03-12 18:38:56 -0400

    created home page, need to link after google oauth

---
+++ college-kitchen/src/home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <header>
+        <h1>Home Page</h1>
+      </header>
+      <main>
+        <img src="https://via.placeholder.com/300" alt="Placeholder" />
+      </main>
+    </div>
+  );
+}
+
+export default Home;
