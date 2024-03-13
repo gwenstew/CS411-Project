fe6ea8e8a548fe6b4b959b92ece2da3bf9e4a68c
gwenstew <gstew@bu.edu>
2024-03-13 12:28:21 -0400

    still fixinf merge error

--- college-kitchen/src/firebase.js
+++ college-kitchen/src/firebase.js
@@ -3,16 +3,6 @@ import { initializeApp } from 'firebase/app';
 import { GoogleAuthProvider, getAuth , signInWithGooglePopup} from "firebase/auth";
 
 const firebaseConfig = {
-<<<<<<< HEAD
-    apiKey: "AIzaSyArNaWNiI7dlCQZi1b9lIEQa4pDsroW6KE",
-    authDomain: "college-kitchen-bu.firebaseapp.com",
-    databaseURL: "https://college-kitchen-bu-default-rtdb.firebaseio.com",
-    projectId: "college-kitchen-bu",
-    storageBucket: "college-kitchen-bu.appspot.com",
-    messagingSenderId: "646410487427",
-    appId: "1:646410487427:web:9381fd24abc60bc7893db6",
-    measurementId: "G-KRB5DSBN00"
-=======
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
@@ -21,7 +11,6 @@ const firebaseConfig = {
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_FIREBASE_APP_ID,
     measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
->>>>>>> 6da2052fe17d4a98b6916ccc5cee485fb9445f7a
   };
   
   // initialize firebase
