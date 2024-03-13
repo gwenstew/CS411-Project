53b7c9bbb5a5b73145e0b3d06a5a91ce98720d1b
gwenstew <gstew@bu.edu>
2024-03-13 12:18:46 -0400

    fixing merge error

--- college-kitchen/src/App.js
+++ college-kitchen/src/App.js
@@ -4,11 +4,8 @@ import './App.css'
 import { useEffect, useState } from 'react'
 import React from 'react';
 import Home from './home';
-<<<<<<< HEAD
 //import SignIn from './googleSignIn';
-=======
-// import SignIn from './googleSignIn';
->>>>>>> 6da2052fe17d4a98b6916ccc5cee485fb9445f7a
+
 // for some reason whenever i try to add the google sign in 
 //button i get node module dependency errors :(( 
 function App() {
