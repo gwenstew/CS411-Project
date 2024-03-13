ca946561aa86d00e4f30b751ab0463b14ca7c6ea
gwenstew <gstew@bu.edu>
2024-03-12 23:06:17 -0400

    adding functionality for google sign in. Not yet connected to react app

---
+++ college-kitchen/src/googleSignIn.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { signInWithPopup } from "firebase/auth";
+import { provider } from "./firebase";
+
+
+function SignIn() {
+    const navigate = useNavigate(); // Initialize useNavigate hook
+  
+    const handleSignIn = () => {
+      signInWithPopup(provider)
+        .then((result) => {
+          const user = result.user;
+          console.log("User signed in:", user);
+          // Redirect the user to the home route
+          navigate('/home'); // Redirect to the home route after successful sign-in
+        })
+        .catch((error) => {
+          console.error("Error signing in:", error);
+        });
+    };
+  
+    return (
+      <button onClick={handleSignIn}>Sign in with Google</button>
+    );
+  }
+  
+  export default SignIn;
