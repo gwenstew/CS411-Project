a2091eaf7cfc0ee41a365a747125be8347172091
ethansliu <esliu@bu.edu>
2024-03-12 22:23:59 -0700

    updated firebase.js firebaseConfig

--- college-kitchen/src/firebase.js
+++ college-kitchen/src/firebase.js
@@ -3,14 +3,14 @@ import { initializeApp } from 'firebase/app';
 import { GoogleAuthProvider, getAuth , signInWithGooglePopup} from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyArNaWNiI7dlCQZi1b9lIEQa4pDsroW6KE",
-    authDomain: "college-kitchen-bu.firebaseapp.com",
-    databaseURL: "https://college-kitchen-bu-default-rtdb.firebaseio.com",
-    projectId: "college-kitchen-bu",
-    storageBucket: "college-kitchen-bu.appspot.com",
-    messagingSenderId: "646410487427",
-    appId: "1:646410487427:web:9381fd24abc60bc7893db6",
-    measurementId: "G-KRB5DSBN00"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
   };
   
   // initialize firebase
