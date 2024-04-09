5e4de2c3a066c8bda65e48041acf44e3ed509dce
Yasmine Jibrell <yjibrell@bu.edu>
2024-04-09 19:06:32 -0400

    more button fixes

--- college-kitchen/src/favorites.js
+++ college-kitchen/src/favorites.js
@@ -82,12 +82,11 @@ function Favorites() {
 
     return (
         <div className="container">
+            <button className='back-home' onClick={() => navigate("/home")}>< i class="ri-home-2-line"></i>
+            </button>
             <div className="profile">
                 <Link to="/profile" className="profile-button">
                     <i className="ri-user-line"></i>
-                </Link>
-                <Link to="/home" className="back-home">
-                    <i className="ri-home-2-line"></i>
                 </Link>
             </div>
             <div className="features-container">
