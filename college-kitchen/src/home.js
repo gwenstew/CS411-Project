8d833af03c7aaddf74a7f7e9bca2108225d785c5
diyagandhi15 <123668392+diyagandhi15@users.noreply.github.com>
2024-04-09 09:33:26 -0400

    added page number near the arrows, changed React App to College kitchen

--- college-kitchen/src/home.js
+++ college-kitchen/src/home.js
@@ -189,10 +189,11 @@ function Home() {
               </div>
               {recipes.length > 0 && (
               <div className='pagination-container'>
-              <div className="pagination">
-                <button className= "more-button" onClick={handlePrevPage} disabled={currentPage === 1}><i class="ri-arrow-left-s-line"></i></button>
-                <button className= "more-button" onClick={handleNextPage} disabled={currentPage === totalPages}><i class="ri-arrow-right-s-line"></i></button>
-              </div>
+                <div className="pagination">
+                  <button className= "more-button" onClick={handlePrevPage} disabled={currentPage === 1}><i class="ri-arrow-left-s-line"></i></button>
+                  <button className= "more-button" onClick={handleNextPage} disabled={currentPage === totalPages}><i class="ri-arrow-right-s-line"></i></button>
+                </div>
+                <div className="page-number">Page {currentPage} of {totalPages}</div>
               </div>
               )}
             </div>
