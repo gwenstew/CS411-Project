9d4481b3d927427caf90a757ce09d12e30dc4812
Yasmine Jibrell <yjibrell@bu.edu>
2024-04-09 17:41:47 -0400

    updates to the favorites page functionality

--- college-kitchen/src/favorites.js
+++ college-kitchen/src/favorites.js
@@ -11,7 +11,13 @@ function Favorites() {
     const user = auth.currentUser;
     const userID = user ? user.uid : null;
     const [favorites, setFavorites] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
+    const [recipes, setRecipes] = useState([]);
+    const [selectedRecipe, setSelectedRecipe] = useState(null);
     // const [recipeArray, setRecipeArray] = useState([]);
+    // const [ingredientArray, setIngredientArray] = useState([]);
+    // const [ingredients, setIngredients] = useState('');
 
     useEffect(() => {
         if (userID) {
@@ -53,23 +59,95 @@ function Favorites() {
         }
     }
 
+    const handleRecipeClick = async (recipeId) => {
+        try {
+          const response = await fetch(`https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=19d968e0a6084103addc8057885c3dfc`);
+          const data = await response.json();
+          setSelectedRecipe(data);
+        } catch (error) {
+          console.error('Error fetching recipe details:', error);
+        }
+      };
+
+    const handleNextPage = () => {
+        setCurrentPage(prevPage => Math.min(prevPage + 1, totalPages));
+    };
+    
+    const handlePrevPage = () => {
+        setCurrentPage(prevPage => Math.max(prevPage - 1, 1));
+    };
+
+    const handleGoBack = () => {
+        setSelectedRecipe(null);
+    };
+
+
+
     return (
         <div className="container">
-            <h2>Favorite Recipes</h2>
-            <div className="favorites-list">
-                {favorites.map((recipe, index) => (
-                    <div key={recipe.id} className="favorite-item">
-                        <Link to={`/recipe/${recipe.id}`}>
-                            <img src={`https://spoonacular.com/recipeImages/${recipe.id}-636x393.jpg`} alt={recipe.title} />
-                        </Link>
-                        <UpdateFavorite recipeId={recipe.id} />
-                        <button className='delete' onClick={() => deleteRecipe(recipe.id)}>Remove from Favorites</button>
-                    </div>
-                ))}
+            <div className="profile">
+            <Link to="/profile" className="profile-button">
+                <i className="ri-user-line"></i>
+                </Link>
             </div>
-            <button className='back-home' onClick={() => navigate("/home")}>Back To Homepage</button>
+            <div className="features-container">
+                <Link to="/pantry" className="pantry-button">
+                    <i className="ri-shopping-basket-line"></i>
+                </Link>
+                <Link to="/home" className="home-button">
+                <i className="ri-home-2-line"></i> 
+                </Link>
+            </div>
+            <div className='favorite-recipes'>
+                {selectedRecipe ? (
+                    <div>
+                        <button className="back-button" onClick={handleGoBack}><i class="ri-arrow-left-s-line"></i></button>
+                        <h2>{selectedRecipe.title}</h2>
+                        <img src={selectedRecipe.image} alt={selectedRecipe.title}/>
+              
+                        <div className='ingredients-container'>
+                        <h3>Ingredients:</h3>
+                        <ul>
+                            {selectedRecipe.extendedIngredients.map(ingredient => (
+                                <li key={ingredient.id}>{ingredient.original}</li>
+                            ))}
+                        </ul>
+                        </div>
+
+                        <div className='instructions'>
+                            <h3>Instructions:</h3>
+                            <div dangerouslySetInnerHTML={{ __html: selectedRecipe.instructions }} />
+                        </div>
+
+                    </div>
+                ) : (
+                    <div>
+                        <h2>Favorite Recipes</h2>
+                        <div className="favorites-list">
+                        {favorites.slice((currentPage - 1) * 10, currentPage * 10).map(recipe => (
+                            <div key={recipe.id} className="favorite-item" onClick={() => handleRecipeClick(recipe.id)}>
+                                <img src={`https://spoonacular.com/recipeImages/${recipe.id}-636x393.jpg`} alt={recipe.title} />
+                                <p>{recipe.title}</p>
+                                
+                                <UpdateFavorite recipeId={recipe.id} />
+                                <button className='delete' onClick={() => deleteRecipe(recipe.id)}>Remove from Favorites</button>
+                            </div>
+                        ))}
+                        </div>
+                    {recipes.length > 0 && (
+                    <div className='pagination-container'>
+                        <div className="pagination">
+                            <button className= "more-button" onClick={handlePrevPage} disabled={currentPage === 1}><i class="ri-arrow-left-s-line"></i></button>
+                            <button className= "more-button" onClick={handleNextPage} disabled={currentPage === totalPages}><i class="ri-arrow-right-s-line"></i></button>
+                        </div>
+                        <div className="page-number">Page {currentPage} of {totalPages}</div>
+                    </div>
+                    )}
+                    </div> 
+                )}
+            </div>
         </div>
     );
-
 }
-export default Favorites;
+
+export default Favorites
